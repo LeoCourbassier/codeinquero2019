8d79cc1326d06412c3f6b85ca4080133e1a62703
Igor Luppi <igor.luppi@gmail.com>
2019-06-02 04:06:25 -0300

    Login basic

---
+++ src/components/login.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Bar from './bar';
+import Footer from './footer';
+import { Row, Col, CardPanel, TextInput, Button } from 'react-materialize';
+// Import Materialize
+import M from "materialize-css";
+
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <Bar ></Bar>
+
+                <CardPanel className="teal" style={styleForm}>
+                    <span className="white-text">
+                        <TextInput label="First Name" />
+                        <TextInput password label="Password" />
+
+                        <div>
+                            <Button waves="light" style={{ marginRight: '5px' }}>
+                                Login
+                            </Button>
+                        </div>
+                    </span>
+                </CardPanel>
+
+                <Footer style={styleFooter}></Footer>
+            </div>
+        );
+    }
+
+
+}
+
+//n funciona
+const styleFooter = {
+    position: "fixed",
+    bottom: "0",
+    left: "0",
+    width: "100%"
+}
+
+const styleForm = {
+    width: "220px",
+    textAlign: "center",
+    position: "center",
+    alignContent: "center"
+}
